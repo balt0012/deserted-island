e7fd998b9f74e887bff2f87a6c07efba2eec8f4b
balt0012 <90677412+balt0012@users.noreply.github.com>
2021-09-19 15:59:27 +0200

    rock paper scissors no draws

--- src/rockPaperScissors.java
+++ src/rockPaperScissors.java
@@ -24,66 +24,89 @@ public class rockPaperScissors {
         Random random = new Random();
         String playerOneChoice;
         String playerTwoChoice;
+        int winner = 0;
+        int roundWinner;
         System.out.println("what is the player's name? ");
         String playerOneName = scanner.nextLine();
-        do{
-            System.out.println(playerOneName + " chose rock, paper or scissors");
-            playerOneChoice = scanner.nextLine();
-        }while ((!playerOneChoice.equals("rock") && !playerOneChoice.equals("paper") && !playerOneChoice.equals("scissors")));
-        int randomChoice = random.nextInt(3) + 1;
-        if(randomChoice == 1){
-            playerTwoChoice = "rock";
+        for (int i = 0; i < 3; i++) {
+            do{
+                do {
+                    System.out.println(playerOneName + " chose rock, paper or scissors");
+                    playerOneChoice = scanner.nextLine();
+                } while ((!playerOneChoice.equals("rock") && !playerOneChoice.equals("paper") && !playerOneChoice.equals("scissors")));
+                int randomChoice = random.nextInt(3) + 1;
+                if (randomChoice == 1) {
+                    playerTwoChoice = "rock";
+                } else if (randomChoice == 2) {
+                    playerTwoChoice = "paper";
+                } else {
+                    playerTwoChoice = "scissors";
+                }
+                System.out.println(playerOneName + " chose " + playerOneChoice + " the computer chose " + playerTwoChoice);
+                roundWinner = playRockPaperScissors(playerOneChoice, playerTwoChoice);
+                if (roundWinner == 1) {
+                    System.out.println("the round winner is " + playerOneName);
+                    winner += 1;
+                } else if (roundWinner == 2) {
+                    System.out.println("the round winner is the computer");
+                    winner -= 1;
+                } else {
+                    System.out.println("the round was a draw");
+                }
+            } while (roundWinner != 0);
         }
-        else if (randomChoice == 2){
-            playerTwoChoice = "paper";
-        }
-        else{
-            playerTwoChoice = "scissors";
-        }
-        System.out.println(playerOneName + " chose " + playerOneChoice + " the computer chose " + playerTwoChoice);
-
-        int winner = rockPaperScissors(playerOneChoice, playerTwoChoice);
-        if (winner == 1){
-            System.out.println("the winner is " + playerOneName);
-        }
-        else if (winner == 2){
-            System.out.println("the winner is the computer");
-        }
-        else {
+        if (winner > 0){
+            System.out.println("the match winner is " + playerOneName);
+        } else if (winner < 0) {
+            System.out.println("the match winner is the computer");
+        } else {
             System.out.println("the match was a draw");
         }
     }
 
-    public static void multiplayerMode(){
+    public static void multiplayerMode() {
         Scanner scanner = new Scanner(System.in);
         String playerOneChoice;
         String playerTwoChoice;
+        int winner = 0;
+        int roundWinner;
         System.out.println("what is player1's name? ");
         String playerOneName = scanner.nextLine();
         System.out.println("what is player2's name? ");
         String playerTwoName = scanner.nextLine();
-        do{
-            System.out.println(playerOneName + " chose rock, paper or scissors");
-            playerOneChoice = scanner.nextLine();
-        }while ((!playerOneChoice.equals("rock") && !playerOneChoice.equals("paper") && !playerOneChoice.equals("scissors")));
-        do {
-            System.out.println(playerTwoName + " chose rock, paper or scissors");
-            playerTwoChoice = scanner.nextLine();
-        }while ((!playerTwoChoice.equals("rock") && !playerTwoChoice.equals("paper") && !playerTwoChoice.equals("scissors")));
-        System.out.println(playerOneName + " chose " + playerOneChoice + " " + playerTwoName + " chose " + playerTwoChoice);
-        int winner = rockPaperScissors(playerOneChoice, playerTwoChoice);
-        if (winner == 1){
-            System.out.println("the winner is " + playerOneName);
+        for (int i = 0; i < 3; i++) {
+            do {
+                do {
+                    System.out.println(playerOneName + " chose rock, paper or scissors");
+                    playerOneChoice = scanner.nextLine();
+                } while ((!playerOneChoice.equals("rock") && !playerOneChoice.equals("paper") && !playerOneChoice.equals("scissors")));
+                do {
+                    System.out.println(playerTwoName + " chose rock, paper or scissors");
+                    playerTwoChoice = scanner.nextLine();
+                } while ((!playerTwoChoice.equals("rock") && !playerTwoChoice.equals("paper") && !playerTwoChoice.equals("scissors")));
+                System.out.println(playerOneName + " chose " + playerOneChoice + " " + playerTwoName + " chose " + playerTwoChoice);
+                roundWinner = playRockPaperScissors(playerOneChoice, playerTwoChoice);
+                if (roundWinner == 1) {
+                    System.out.println("the round winner is " + playerOneName);
+                    winner += 1;
+                } else if (roundWinner == 2) {
+                    System.out.println("the round winner is " + playerTwoName);
+                    winner -= 1;
+                } else {
+                    System.out.println("the match was a draw, redo!");
+                }
+            } while (roundWinner != 0);
         }
-        else if (winner == 2){
-            System.out.println("the winner is " + playerTwoName);
-        }
-        else {
+        if(winner > 0){
+            System.out.println("the match winner is " + playerOneName);
+        } else if (winner < 0) {
+            System.out.println("the match winner is " + playerTwoName);
+        } else {
             System.out.println("the match was a draw");
         }
     }
 
-    public static int rockPaperScissors(String playerOneChoice, String playerTwoChoice){
+    public static int playRockPaperScissors(String playerOneChoice, String playerTwoChoice){
         if (playerOneChoice.equals("rock") && playerTwoChoice.equals("paper")){
             return 2;
         }
