502b8468c4206ba8bc182c3e27b2a5acdd5bfc23
balt0012 <90677412+balt0012@users.noreply.github.com>
2021-09-19 10:16:13 +0200

    finnished project

--- src/desertedIsland.java
+++ src/desertedIsland.java
@@ -10,9 +10,31 @@ public class desertedIsland {
             System.out.println(i+1 + " " + options[i]);
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("indtast valg: ");
+            System.out.print("indtast valg: ");
             int userChoice = scanner.nextInt();
+            System.out.println(options[userChoice-1]);
             choiceFrequency[userChoice - 1] += 1;
+
         }
+        System.out.print("[" + choiceFrequency[0]);
+        for (int i = 1; i < 10; i++) {
+            System.out.print(", " + choiceFrequency[i]);
+        }
+        int highNumber = highestNumber(choiceFrequency);
+        System.out.println("]");
+        System.out.println("mest populære valg er " + options[highNumber] + " " + choiceFrequency[highNumber]);
+    }
+
+    public static int highestNumber (int[] arrayToCheck){
+        int highNumber = 0;
+        for (int i = 1; i < arrayToCheck.length; i++) {
+            if (arrayToCheck[i] > arrayToCheck[i-1]){
+                highNumber = i;
+            }
+            else {
+                arrayToCheck[i] = arrayToCheck[i-1];
+            }
+        }
+        return highNumber;
     }
 }
---
+++ src/rockPaperScissors.java
@@ -0,0 +1,5 @@
+public class rockPaperScissors {
+    public static void main(String[] args) {
+
+    }
+}
